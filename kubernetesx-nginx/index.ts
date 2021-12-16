69959f4bc38265236d10709602cabb2e799add7b
David Flanagan <david@rawkode.com>
2021-12-16 11:55:52 +0000

    fix: expose ports

--- kubernetesx-nginx/index.ts
+++ kubernetesx-nginx/index.ts
@@ -4,10 +4,13 @@ const nginxPod = new kx.PodBuilder({
   containers: [
     {
       image: "nginx:1.21",
+      ports: [{ containerPort: 80 }],
     },
   ],
 });
 
 const nginxDeployment = new kx.Deployment("nginx", {
   spec: nginxPod.asDeploymentSpec(),
-}).createService({});
+}).createService({
+  ports: [{ port: 80, targetPort: 80 }],
+});
